b555fbb3de5ec284beeb877cbae84fa08467f63e
Chris McArthur <prince.chrismc@gmail.com>
2019-04-07 20:50:05 -0400

    playing with props in react

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
+var MessageComponent = React.createClass({
+  render: function() {
+    return (
+      <div>{this.props.message}</div>
+    );
+  }
+});
+
+var VacancySign = React.createClass({
+  render: function() {
+    var text;
+    if (this.props.hasvacancy) {
+      text = 'Vacancy';
+    } else {
+      text = 'No Vacancy';
+    }
+    return <MessageComponent message={text}/>;
+  }
+});
+
 export default class App extends React.Component {
   render() {
     return (<div>
       <h2>Welcome to React!</h2>
+      <VacancySign hasvacancy={false} />
     </div>);
   }
 }
