e83e8e544cf161e5436835330c66679e6f22b267
Chris McArthur <prince.chrismc@gmail.com>
2019-04-08 19:28:41 -0400

    playing with grid from material-ui

--- src/app.jsx
+++ src/app.jsx
@@ -3,10 +3,6 @@ import { VacancySign } from './components/vacancy_sign'
 
 export default class App extends React.Component {
   render() {
-    return (
-    <div>
-      <h2>Welcome to React!</h2>
-      <VacancySign hasvacancy={false} />
-    </div>);
+    return <VacancySign hasvacancy={false} />;
   }
 }
--- src/components/vacancy_sign.jsx
+++ src/components/vacancy_sign.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { MessageComponent } from './message'
-import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
-const ToogleComponent = ({onToggle}) => {
+const ToogleComponent = ({ onToggle }) => {
   return (
     <div>
-      <Button variant="contained" color="primary" onClick={() => {onToggle("item")}}>
+      <Button variant="contained" color="primary" onClick={() => { onToggle("item") }}>
         Toggle
       </Button>
     </div>
   );
- }
+}
 
 export class VacancySign extends React.Component {
   constructor(props) {
@@ -23,8 +24,8 @@ export class VacancySign extends React.Component {
 
   toggle() {
     this.setState((prevState) => (
-        this.state, 
-        { isVacant: !prevState.isVacant }
+      this.state,
+      { isVacant: !prevState.isVacant }
     ));
   }
 
@@ -37,9 +38,20 @@ export class VacancySign extends React.Component {
     }
     return (
       <div>
-        <MessageComponent message={text} />
-        <hr/>
-        <ToogleComponent onToggle={this.toggle.bind(this)} />
+        <Grid container spacing={24} justify="space-around" alignItems="center">
+          <Grid item xs={10}>
+            <h2>Motel Electify</h2>
+          </Grid>
+          <Grid item xs={10}>
+            <Paper>
+              <div style={{ padding: 20 }}>
+                <MessageComponent message={text} />
+                <hr />
+                <ToogleComponent onToggle={this.toggle.bind(this)} />
+              </div>
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     );
   }
